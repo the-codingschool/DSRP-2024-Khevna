2262de57d20ff5e0815918437c5b2c88734fec72
HexaA2 <hexatonysong@gmail.com>
2024-07-22 14:21:26 -0400

    Added theme

--- tonyWork/finalRShinyApp.R
+++ tonyWork/finalRShinyApp.R
@@ -2,23 +2,23 @@ library(ggplot2)
 library(dplyr)
 library(tidyverse)
 library(shiny)
+library(shinythemes)
 library(tibble)
 library(vroom)
 
 ui <- fluidPage(theme = shinytheme("sandstone"),
   navbarPage(title = "Lung Cancer Data Science - Tony",
-             tabPanel("Introduction",
-                      h2("About the Dataset"),
-                      p("Realistic python generated dataset on lung cancer patients taken from kaggle. 38 variables, 23658 patients"),
-                      h2("What I did"),
-                      h2("About Me")
-                      
-                      
-                      )
+     tabPanel("Introduction",
+        h2("About the Dataset"),
+        p("Realistic python generated dataset on lung cancer patients taken from kaggle."),
+        h4("38 variables, 23658 patients"),
+        h2("What I did"),
+        h2("About Me")
+     )
              
              
              
-             )
+  )
   
 )
 server <- function(input,output){}
