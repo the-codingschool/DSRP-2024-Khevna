d5bfbbe537023288dcf6e95cab3aa8ebbded408f
HexaA2 <hexatonysong@gmail.com>
2024-07-22 13:59:20 -0400

    obtain data from CSV file

---
+++ getCsv.R
@@ -0,0 +1,16 @@
+library(utils)
+library(ggcorrplot)
+library(dplyr)
+# Get csv file
+
+data <- read.csv("lung_cancer_data.csv")
+
+# We want to see which variables are most correlated with each other
+
+numericalData = select(data,c("Age", "Tumor_Size_mm","Survival_Months","Blood_Pressure_Systolic","Blood_Pressure_Diastolic",
+                              "Blood_Pressure_Pulse","Hemoglobin_Level","White_Blood_Cell_Count","Platelet_Count",
+                              "Albumin_Level","Alkaline_Phosphatase_Level","Alanine_Aminotransferase_Level",
+                              "Aspartate_Aminotransferase_Level","Creatinine_Level","LDH_Level","Calcium_Level",
+                              "Phosphorus_Level","Glucose_Level","Potassium_Level","Sodium_Level","Smoking_Pack_Years"))
+
+ggcorrplot(cor(numericalData))
