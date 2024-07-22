c6968c52ab78f20eb5bd6e316d20e282aed82f84
HexaA2 <hexatonysong@gmail.com>
2024-07-22 14:16:52 -0400

    Moved stuff between folders

---
+++ tonyWork/finalRShinyApp.R
@@ -0,0 +1,26 @@
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+library(shiny)
+library(tibble)
+library(vroom)
+
+ui <- fluidPage(theme = shinytheme("sandstone"),
+  navbarPage(title = "Lung Cancer Data Science - Tony",
+             tabPanel("Introduction",
+                      h2("About the Dataset"),
+                      p("Realistic python generated dataset on lung cancer patients taken from kaggle. 38 variables, 23658 patients"),
+                      h2("What I did"),
+                      h2("About Me")
+                      
+                      
+                      )
+             
+             
+             
+             )
+  
+)
+server <- function(input,output){}
+
+shinyApp(ui=ui,server=server)
