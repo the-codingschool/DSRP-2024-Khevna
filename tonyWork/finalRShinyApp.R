6007fd498b48168b04275672ab0a6c1383d7e882
HexaA2 <hexatonysong@gmail.com>
2024-07-22 14:33:58 -0400

    Added more tabs to shiny app

--- tonyWork/finalRShinyApp.R
+++ tonyWork/finalRShinyApp.R
@@ -7,20 +7,50 @@ library(tibble)
 library(vroom)
 
 ui <- fluidPage(theme = shinytheme("sandstone"),
-  navbarPage(title = "Lung Cancer Data Science - Tony",
-     tabPanel("Introduction",
-        h2("About the Dataset"),
-        p("Realistic python generated dataset on lung cancer patients taken from kaggle."),
-        h4("38 variables, 23658 patients"),
-        h2("What I did"),
-        h2("About Me")
-     )
-             
-             
-             
-  )
+    navbarPage(title = "Lung Cancer Data Science - Tony",
+         tabPanel("Introduction",
+              h2("About the Dataset"),
+              p("Realistic python generated dataset on lung cancer patients taken from kaggle."),
+              h4("38 variables, 23658 patients"),
+              h2("What I did"),
+              h2("About Me")
+         ),
+         navbarMenu("Custom Data",
+              tabPanel("Import dataset",
+                  h2("Use your own dataset (Not Recommended)"),
+                  p("Make sure your dataset has all the same columns as the default dataset!"),
+                  p("If your dataset contains different variable names, please define them in the variable declaration tab"),
+                  
+                  fileInput("inputCSV", "Choose Custom CSV File",
+                      accept = c(
+                        "text/csv",
+                        "text/comma-separated-values,text/plain",
+                        ".csv"
+                      )
+                  )
+              ),
+              tabPanel("Declare variables",
+                  h2("Declare your own variable names."),
+                  p("Note that Patient_ID is not necessary")
+              )
+          )
+               
+               
+               
+    )
   
 )
-server <- function(input,output){}
+server <- function(input,output){
+  
+  dataInput <- reactive ({
+    if(is.null(input$inputCSV)){
+      df <- read.csv("lung_cancer_data.csv")
+    }
+    
+    
+  })
+  
+  
+}
 
 shinyApp(ui=ui,server=server)
