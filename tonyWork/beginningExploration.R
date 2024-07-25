bd416daf55fb2be4c72b92344ee620915e9a45e9
HexaA2 <hexatonysong@gmail.com>
2024-07-25 15:56:42 -0400

    Added more graphs

--- tonyWork/beginningExploration.R
+++ tonyWork/beginningExploration.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(dplyr)
 library(tidyr)
 library(reshape2)
+library(gridExtra)
 
 # Get csv file
 # 1. Read the data from a CSV file and set working directory (This may be different for you)
@@ -67,14 +68,12 @@ hist(data$Survival_Months)
 # Plot each numerical variable as a boxplot to see if it is just boxplot being normal or not
 
 par(mfrow = c(3, 7))  # 3 rows and 7 columns
-
-
 for (i in 1:21) {
   boxplot(numericalData[, i], main = colnames(numericalData)[i], col = "lightblue")
 }
 
 for (i in 1:21) {
-  hist(numericalData[, i], main = colnames(numericalData)[i], col = rainbow(10))
+  hist(numericalData[, i], main = colnames(numericalData)[i], col = paletteer_d("RColorBrewer::Pastel2"))
 }
 # You can see that almost everything is evenly distributed. Some of them have extra or half on the ends.
 # For example, Blood_Pressure_Pulse has double in the first bin and half in the last.
@@ -102,6 +101,7 @@ ggplot(longData, aes(x = Ethnicity, y = Value)) +
 # Scatter plots for age vs other values colored by ethnicity
 
 # Create and save individual scatter plots
+par(mfrow = c(1, 1))
 ggplot(numericalDataEthnicity, aes(x = Age, y = Tumor_Size_mm, color = Ethnicity)) +
   geom_point() +
   theme_bw() +
@@ -123,15 +123,16 @@ long_data <- melt(data, variable.name = "Variable", value.name = "Value")
 
 ggplot(long_data, aes(x = Stage, y = Value)) +
   geom_boxplot() +
-  facet_wrap(~ Variable, scales = "free_y", ncol = 5)
+  facet_wrap(~ Variable, scales = "free_y", ncol = 5) +
 theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
-labs(title = "Boxplots of Numerical Variables",
-     x = "Variable",
-     y = "Value")
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),plot.title = element_text(hjust = 0.5)) +
+labs(title = "Boxplots of Numerical Variables vs Stage",
+     x = "Stage",
+     y = "Numerical Variable Value") 
 
 ggplot(data,aes(x=Treatment,y=Survival_Months)) +
-  geom_boxplot()
+  geom_boxplot() + labs(title="Boxplot of Survival Months vs Treatment") +
+  theme(plot.title = element_text(hjust = 0.5))
 
 par(mfrow = c(2, 4))
 for(i in 13:19) {
@@ -144,4 +145,15 @@ data$Survival <- as.factor(data$Survival)
 ggplot(data,aes(x=Survival,y=Age,group=Survival)) + geom_boxplot() + labs(title = "Boxplot of Age by Survival Group",
                                                                x = "Survival Group",
                                                                y = "Age")
+
+plot_list <- list()
+for(comorbidity in c("Comorbidity_Diabetes", "Comorbidity_Hypertension", "Comorbidity_Heart_Disease", "Comorbidity_Chronic_Lung_Disease", "Comorbidity_Kidney_Disease", "Comorbidity_Autoimmune_Disease", "Comorbidity_Other")){
+  p <- ggplot(data, aes_string(x = "Stage", y = "Survival_Months", fill = comorbidity)) +
+    geom_boxplot() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  
+  plot_list[[comorbidity]] <- p
+}
+do.call(grid.arrange, c(plot_list, ncol = 2))
+
  
