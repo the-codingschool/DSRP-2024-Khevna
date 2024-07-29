51351e40f196b98e9f88ccc75f3be40f239588ce
HexaA2 <hexatonysong@gmail.com>
2024-07-29 12:39:04 -0400

    boxplots for numerical

--- tonyWork/beginningExplorationDataset2.R
+++ tonyWork/beginningExplorationDataset2.R
@@ -25,4 +25,23 @@ correlation_matrix <- cor(numericalData,use = "complete.obs")
 
 ggcorrplot(correlation_matrix)
 
-
+plot_list <- list()
+for (variable in c("radius_mean","texture_mean","perimeter_mean",
+                   "area_mean","smoothness_mean","compactness_mean",
+                   "concavity_mean","concave.points_mean","symmetry_mean",
+                   "fractal_dimension_mean","radius_se","texture_se","perimeter_se",
+                   "area_se","smoothness_se","compactness_se","concavity_se",
+                   "concave.points_se","symmetry_se","fractal_dimension_se",
+                   "radius_worst","texture_worst","perimeter_worst","area_worst",
+                   "smoothness_worst","compactness_worst","concavity_worst",
+                   "concave.points_worst","symmetry_worst","fractal_dimension_worst")) {
+  p <- ggplot(data, aes_string(x="diagnosis",y = variable)) + 
+    geom_boxplot() + 
+    geom_jitter(aes(color=diagnosis),width = 0.2, height = 0, alpha = 0.6) +
+    theme_minimal() +
+    theme(axis.text.x = element_blank(),  # Remove x-axis text
+          axis.ticks.x = element_blank(),  # Remove x-axis ticks
+          legend.position = "none")
+  plot_list[[variable]] <- p
+}
+do.call(grid.arrange, c(plot_list, ncol = 10))
